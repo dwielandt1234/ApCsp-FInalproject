05b3deca5bd6ebcc3a0fbb67079de21410e81551
dwielandt1234 <86891010+dwielandt1234@users.noreply.github.com>
2023-11-06 15:54:40 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
+import speech_recognition as spr
+from googletrans import Translator
+from gtts import gTTS
+import os
 
+#define voice variables, create a microphone
+Reco = spr.Recognizer()
+micro = spr.Microphone()
+#capture the voice 
+with mc as source:
+  print("Hey Please say 'Hello' in order to start the Recognizion")
+  print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+  Reco.adjust_for_ambient_noise(source, duration=0.2)
+  audio = Reco.listen(source) 
+  Mytext = Reco.recognize_google(audio) 
+
+#this will ask the user if what we have is what he said
+print("You said: ", Mytext)
+fcheck = input("Is this correct? (y/n): ")
